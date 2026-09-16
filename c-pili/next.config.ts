1fd0c575797f6000f3f9b02041b4fd5c9d5af072
Clement <Clement.pili@etu.univ-smb.fr>
2026-09-16 23:01:07 +0200

    Update next.config.ts

--- c-pili/next.config.ts
+++ c-pili/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "export",
     images: {
+        unoptimized: true, // Requis pour l'export statique sur GitHub Pages
         remotePatterns: [
             {
                 protocol: "https",
